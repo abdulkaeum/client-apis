a812ac98afad407fc7619d5430ef64658d3178a8
abdulkaeum <abdul.kaeum@hotmail.com>
2022-02-28 09:47:52 +0000

    migrations and model

--- database/migrations/2022_02_28_094056_create_clients_table.php
+++ database/migrations/2022_02_28_094056_create_clients_table.php
@@ -13,15 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('clients', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->unsignedBigInteger('client_id')->unique()->index();
-            $table->string('avatar')->nullable();
-            $table->rememberToken();
+            $table->string('name')->unique();
+            $table->string('base_uri');
             $table->timestamps();
         });
     }
@@ -33,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('clients');
     }
 };
