5902593822f0d0ce6e7019d283c1a2d26a817e5f
abdulkaeum <abdul.kaeum@hotmail.com>
2022-02-28 09:54:28 +0000

    add artisan command

---
+++ app/Console/Commands/ClientApiCommmand.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Client;
+use Illuminate\Console\Command;
+
+class ClientApiCommmand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'client:api {action=index : use index to retrieve or store to persist the API}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Retrieve or persist the Client API';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        Client::all()->each->handle($this->argument('action'));
+    }
+}
